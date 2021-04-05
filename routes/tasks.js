0875fb41536a741056e6b2ff73ac27349d4b8714
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-05 16:38:02 +0300

    Implement task deletion.

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,53 +1,50 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const uuid = require("uuid");
 const router = express.Router();
+const faker = require("faker");
 //muutos
-var taskList = [
-    {
-        "name": "Vacuuming",
-        "note": "Vacuum under sofa",
-        "points": 20,
-        "room": "Livingroom"
-    }, 
-    {
-        "name": "Empty trash",
-        "note": "Empty kitchen trash can",
-        "points": 10,
-        "room": "Kitchen"
+
+const NUM_TASKS = 50;
+
+let tasks = Array.from({ length: NUM_TASKS }).reduce((acc, _) => {
+  const id = uuid.v4();
+  return {
+    ...acc,
+    [id]: {
+      id,
+      name: faker.random.arrayElement([
+        "Empty trash",
+        "Dust windowsills",
+        "Dust baseboards",
+        "Sort out-of-place items",
+        "Make beds",
+        "Carry laundry",
+        "Carry laundry",
+        "Vacuuming",
+      ]),
+      note: faker.lorem.sentence(),
+      points: faker.datatype.number({ min: 10, max: 30 }),
+      room: faker.random.arrayElement([
+        "kitchen",
+        "living room",
+        "restroom",
+        "bedroom",
+      ]),
     },
-    {
-        "name": "Wash dishes",
-        "note": "Washe dishes at kitchen",
-        "points": 30,
-        "room": "Kitchen"
-    },
-    {
-        "name": "Dust",
-        "note": "Dust tables",
-        "points": 15,
-        "room": "Livingroom"
-    },
-    {
-        "name": "Vacuuming",
-        "note": "Vacuum bedroom",
-        "points": 20,
-        "room": "Bedroom"
-    }]
+  };
+}, {});
 
-router.get('/', (req, res) => {
-    res.json(taskList)
-})
+router.get("/", (req, res) => {
+  res.json(Object.values(tasks));
+});
 
-router.post('/', (req, res) => {
-    
-})
+router.post("/", (req, res) => {});
 
-router.put('/', (req, res) => {
-    
-})
+router.put("/", (req, res) => {});
 
-router.delete('/', (req, res) => {
-    
-})
+router.delete("/:id", ({ params: { id: taskId } }, res) => {
+  delete tasks[taskId];
+  res.status(204).end();
+});
 
 module.exports = router;
