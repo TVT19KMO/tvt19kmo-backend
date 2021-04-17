4117b27e7ca642b2b23a404fae78ef2ec883ed5d
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-17 16:54:48 +0300

    Add stripe webhook setup.

--- app/utils/stripe.js
+++ app/utils/stripe.js
@@ -1,5 +1,36 @@
-const stripe = require("stripe")(
-  "sk_test_51Iebt0EEKvNzBMMmZOMLFLT6BXimnFP1GZiTtUZX9eYOq8tQBEYys0ZF7LmBcnXpRN73Y6Bc0PfuRjrISExlxIT100nx1YPDIF"
-);
+const express = require("express");
+const stripe = require("stripe");
 
-module.exports = stripe;
+/**
+ * This module contains some Stripe
+ * specific setup and middlewares.
+ *
+ * @module utils/stripe
+ */
+
+const STRIPE_KEY =
+  "sk_test_51Iebt0EEKvNzBMMmZOMLFLT6BXimnFP1GZiTtUZX9eYOq8tQBEYys0ZF7LmBcnXpRN73Y6Bc0PfuRjrISExlxIT100nx1YPDIF";
+
+const WEBHOOK_KEY = "whsec_EVszXtqERaVy6di0Z3yKFJf0g05plj0R";
+
+/**
+ * Creates a middleware for handling
+ * Stripe webhook events.
+ *
+ * @middleware
+ * @type {import('express').RequestHandler}
+ */
+const webhook = (req, res, next) => {
+  if (req.originalUrl.startsWith("/api/payments/webhook")) {
+    next();
+  } else {
+    express.json()(req, res, next);
+  }
+};
+
+module.exports = {
+  STRIPE_KEY,
+  WEBHOOK_KEY,
+  stripe: stripe(STRIPE_KEY),
+  webhook,
+};
