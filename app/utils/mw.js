a8692db78cc7c0300dc7103474383eb5ad853680
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2021-04-17 16:58:10 +0300

    Add some useful middlewares.

--- app/utils/mw.js
+++ app/utils/mw.js
@@ -1,5 +1,10 @@
 // @ts-check
 
+/**
+ * This module contains some commonly used middlewares.
+ * @module utils/middleware
+ */
+
 const {
   conflict,
   server,
@@ -12,6 +17,7 @@ const {
 /**
  * General-purpose error handler middleware.
  *
+ * @middleware
  * @type {import('express').ErrorRequestHandler}
  * @param {Object} error;
  */
@@ -44,24 +50,31 @@ const errorHandler = (error, _, res, next) => {
           break;
       }
       break;
+
     // Triggers when the user tries to upload invalid amount of files,
     // invalid fields or invalid type of files.
     case "MulterError":
+
     // Triggers when there is some extanous fields in request.
     case "StrictModeError":
+
     // Triggers for invalid data format.
     case "SyntaxError":
+
     // Triggers for invalid request body.
     case badRequest.strCode:
       res
         .status(badRequest.code)
         .json({ message: badRequest.message, detail: error.message });
       break;
+
     // Triggers when JSON web token cannot be fetched.
-    case "JsonWebTokenError": 
+    case "JsonWebTokenError":
+
     // User is not authenticated.
     case unauthorized.strCode:
       res.status(unauthorized.code).json({ message: unauthorized.message });
+
     // Triggers when MongoDB tries to use invalid ID or cannot convert.
     case "CastError":
       // Invalid datatype in body...
@@ -69,14 +82,17 @@ const errorHandler = (error, _, res, next) => {
         res.status(badRequest.code).json({ message: badRequest.message });
         break;
       }
+
     // Nonexisting resource/path.
     case notFound.strCode:
       res.status(notFound.code).json({ message: notFound.message });
       break;
+
     // Unauthorized user.
     case forbidden.strCode:
       res.status(forbidden.code).json({ message: forbidden.message });
       break;
+
     // Unknown erros.
     default:
       //logger.i(error.name);
@@ -86,4 +102,26 @@ const errorHandler = (error, _, res, next) => {
   next();
 };
 
-module.exports = { errorHandler };
+/**
+ * Simple middleware to attach some paths to a request.
+ *
+ * @middleware
+ * @type {import('express').RequestHandler}
+ */
+const pathProvider = (req, _, next) => {
+  req["hostPath"] = `${req.protocol}://${req.get("host")}`;
+  req["fullPath"] = `${req["hostPath"]}${req.originalUrl}`;
+  next();
+};
+
+/**
+ * Handles unknown API endpoints.
+ *
+ * @middleware
+ * @type {import('express').RequestHandler}
+ */
+const unknownHandler = (_, res) => {
+  res.status(notFound.code).send({ message: notFound.message });
+};
+
+module.exports = { errorHandler, unknownHandler, pathProvider };
