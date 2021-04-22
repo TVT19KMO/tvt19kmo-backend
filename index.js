5247f14650f15c692e0758dd0237ae553e2041b8
THeikki <t9tohe00@students.oamk.fi>
2021-04-22 21:20:14 +0300

    added store route

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const {
   assignedTasks,
 } = require("./routes");
 
+const storeRoute = require("./routes/store");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -41,6 +43,7 @@ app.use("/api/payments", payments);
 app.use("/api/products", products);
 app.use("/api/children", children);
 app.use("/api/assigned-tasks", assignedTasks);
+app.use("/api/store", storeRoute)
 app.use("/api", misc);
 
 app.use(mw.unknownHandler);
