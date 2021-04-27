ca927b6dfa8e7db68bc501a192a9263f37902b95
THeikki <t9tohe00@students.oamk.fi>
2021-04-27 21:10:37 +0300

    updated statistic get request

--- routes/statistics.js
+++ routes/statistics.js
@@ -4,18 +4,18 @@ const AssignedTask = require("../models/assignedTask");
 const router = express.Router();
 
 /*
-*** get all assigned tasks
-
-We can calculte number of assigned tasks and completed tasks, coins parent has spended creating tasks, children coin balance,
-
-most popular task room, most popular assignee and most popular task difficulty and task name
+*** get assigned tasks by id
 */
-router.get('/', (req, res) => {
-    AssignedTask.find()
-    .populate('task', { difficulty: 1, name: 1, note:1 })
-    .populate('assignee', { balance: 1, name: 1 })
-    .then(stats => res.status(200).json(stats))
-    .catch(error => res.status(400).json({message: error}))
-})
-
+router.get('/:id', (req,res) => {
+    
+    const parentId = req.params.id
+    
+    AssignedTask.find({assigner: parentId}) 
+        .populate('task', { difficulty: 1, name: 1, note:1 })
+        .populate('assignee', { balance: 1, name: 1 })
+        .then(stats => res.status(200).json(stats))
+        .catch(error => res.status(400).json({message: error}))
+    })  
+    
 module.exports = router;
+
